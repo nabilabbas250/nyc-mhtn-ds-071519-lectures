7686572a18f122789603591b829f22693d7eab28
Nabil <nma320@nyu.edu>
2019-07-24 10:02:49 -0400

    save

---
+++ week-2/SQL/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 15:16:37 2019
+
+@author: flatironschool
+"""
+
+host = 'flatiron.ctd127ny6vxi.us-east-1.rds.amazonaws.com'
+user = 'nabilabbas250'
+password = 'nma18173'
