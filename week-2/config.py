ba124aa47992f7d1e01167662d65d915bee1a58f
Nabil <nma320@nyu.edu>
2019-07-29 10:04:10 -0400

    api_lab_copy_finalized

---
+++ week-2/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 15:16:37 2019
+
+@author: flatironschool
+"""
+
+host = 'flatiron.ctd127ny6vxi.us-east-1.rds.amazonaws.com'
+user = 'nabilabbas250'
+password = 'nma18173'
+api_key = 'WMgB0oJS5hb-hZqNk4SxrM5YOv-WVzabBsyO1InWv-5_esbUAR2pKavVcXBRWiLYcLmrVukepWZGwyhZUP2VvSMhNpMuUonlpLAiUJlqlHFW3nCgP0LzzzC_3Rk6XXYx'
+client_id = 'ZHIPHezpOPB8hoFciU-7rQ'
