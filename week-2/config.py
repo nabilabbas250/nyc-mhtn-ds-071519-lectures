40e409416394f7a4c643e38c396975498cdef120
Nabil <nma320@nyu.edu>
2019-07-29 19:56:22 -0400

    save changes

--- week-2/config.py
+++ week-2/config.py
@@ -8,6 +8,6 @@ Created on Tue Jul 23 15:16:37 2019
 
 host = 'flatiron.ctd127ny6vxi.us-east-1.rds.amazonaws.com'
 user = 'nabilabbas250'
-password = 'nma18173'
+password = 'kalsoom66'
 api_key = 'WMgB0oJS5hb-hZqNk4SxrM5YOv-WVzabBsyO1InWv-5_esbUAR2pKavVcXBRWiLYcLmrVukepWZGwyhZUP2VvSMhNpMuUonlpLAiUJlqlHFW3nCgP0LzzzC_3Rk6XXYx'
 client_id = 'ZHIPHezpOPB8hoFciU-7rQ'
