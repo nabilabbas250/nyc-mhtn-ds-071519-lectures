91bd3aa0ddd9daeaf51a400a40b9270bcab5f6f9
Nabil <nma320@nyu.edu>
2019-08-01 10:09:53 -0400

    saves

---
+++ week-3/Rami /config.py
@@ -0,0 +1,12 @@
+api_key = '6b07ed055b275066e1c75fa784bba848'
+
+example_api_request = 'https://api.themoviedb.org/3/movie/550?api_key=6b07ed055b275066e1c75fa784bba848'
+
+api_read_access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjA3ZWQwNTViMjc1MDY2ZTFjNzVmYTc4NGJiYTg0OCIsInN1YiI6IjVkM2YyNWQ5MjE2MjFiMDAxMmY0Y2UwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8I7LxFtUhmFDcUbRyUeRxyKM9phUT-YZuuNzCS1VMH8'
+
+
+host = 'flatiron.ctd127ny6vxi.us-east-1.rds.amazonaws.com'
+user = 'nabilabbas250'
+password = 'kalsoom66'
+
+base_url = 'https://api.themoviedb.org/3'
